e71c6ec7ee5eacfb66da1eb9f2c5aac1e86e3185
Zhao Kang <zhaokang.cn@gmail.com>
2017-02-02 14:39:43 +0800

    implement CG3dTranslateTF

--- pycg3d/cg3d_transformer.py
+++ pycg3d/cg3d_transformer.py
@@ -1,5 +1,6 @@
 from pycg3d import cg3d_vector
 from pycg3d import cg3d_plane
+from pycg3d import cg3d_cords
 from pycg3d import utils
 
 
@@ -38,11 +39,32 @@ class CG3dRotateTF(CG3dTransformer):
 
 
 class CG3dTranslateTF(CG3dTransformer):
-    def __init__(self, tx=0.0, ty=0.0, tz=0.0):
+    def __init__(self, tx=0.0, ty=0.0, tz=0.0, cords=None):
+        """
+
+        :param tx: movement along X-axis
+        :param ty: movement along Y-axis
+        :param tz: movement along Z-axis
+        :param cords: local coordinate system for the specified movement (None means global system)
+        """
         super(CG3dTranslateTF, self).__init__()
         self._tx = tx
         self._ty = ty
         self._tz = tz
+        self._cords = cords
+
+        self._dv = None
+
+    def get_dv(self):
+        if self._dv is None:
+            dv = cg3d_vector.CG3dVector(self._tx, self._ty, self._tz)
+            if self._cords:
+                assert isinstance(self._cords, cg3d_cords.CG3dCordsBase)
+                dv = self._tx * self._cords.ex + \
+                     self._ty * self._cords.ey + \
+                     self._tz * self._cords.ez
+            self._dv = dv
+        return self._dv
 
     def applyto(self, point):
-        return point + cg3d_vector.CG3dVector(self._tx, self._ty, self._tz)
+        return point + self.get_dv()
