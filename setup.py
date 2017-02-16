8013575fc5b66909c507447b43260eff60184a69
Zhao Kang <zhaokang.cn@gmail.com>
2017-02-17 01:05:01 +0800

    fix description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1',
 
-    description='A package for 3D Computation Geometry',
+    description='A pure Python package for Computational Geometry in 3D',
     long_description=long_description,
 
     # The project's main homepage.
