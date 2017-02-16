b68502e64274533e425a5b6223658a752de49585
Zhao Kang <zhaokang.cn@gmail.com>
2017-02-17 00:58:12 +0800

    update comments

--- pycg3d/utils.py
+++ pycg3d/utils.py
@@ -6,18 +6,30 @@ from pycg3d import cg3d_plane
 def distance(p1, p2):
     """
     compute distance between two points
-    :param p1: a list with 3 floats or Point or CG3dVector
-    :param p2: a list with 3 floats or Point or CG3dVector
-    :return:
+    :param p1: point 1 defined as a list with 3 floats or CG3dPoint
+    :param p2: point 2 defined as a list with 3 floats or CG3dPoint
+    :return: float
     """
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)
 
 
 def dot_product(v1, v2):
+    """
+    compute dot production of two vectors
+    :param v1: vector 1 (a list with 3 floats or CG3dVector)
+    :param v2: vector 2 (a list with 3 floats or CG3dVector)
+    :return: float
+    """
     return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
 
 
 def cross_product(v1, v2):
+    """
+    compute cross production of two vectors
+    :param v1: vector 1 (a list with 3 floats or CG3dVector)
+    :param v2: vector 2 (a list with 3 floats or CG3dVector)
+    :return: CG3dVector
+    """
     return cg3d_vector.CG3dVector(
         v1[1] * v2[2] - v2[1] * v1[2],
         v1[2] * v2[0] - v2[2] * v1[0],
@@ -27,10 +39,10 @@ def cross_product(v1, v2):
 
 def mirror_point_to_plane(point, plane):
     """
-
-    :param point: input point
-    :param plane: mirror plane
-    :return: mirrored point
+    compute mirrored point to a plane
+    :param point: input point (CG3dPoint)
+    :param plane: mirror plane (CG3dPlane)
+    :return: CG3dPoint
     """
     assert isinstance(plane, cg3d_plane.CGPlane)
     pn, norm = plane.get_point_and_normal()
