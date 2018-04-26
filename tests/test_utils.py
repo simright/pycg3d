04e89b9e765c63ed1f369fc73ad04715ff91046b
kang <kang@simright.com>
2018-04-26 12:36:49 +0800

    add vlength() and compute_angle_v2v()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,10 +6,34 @@ from pycg3d import utils
 
 
 class TestUtils(unittest.TestCase):
+    def test_vlenth(self):
+        self.assertAlmostEqual(
+            utils.vlength([1.0, 2.0, 3.0]),
+            3.7416573867739413,
+            delta=0.001
+        )
+
     def test_distance(self):
         self.assertEqual(
             utils.distance(CG3dPoint(1.0, 2.0, 3.0), CG3dPoint(4.0, 1.0, 2.0)),
             math.sqrt(11.0)
+        )
+
+    def test_compute_angle_v2v(self):
+        self.assertEqual(
+            utils.compute_angle_v2v(
+                [1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0]
+            ),
+            0.5*math.pi
+        )
+
+        self.assertEqual(
+            utils.compute_angle_v2v(
+                [1.0, 0.0, 0.0],
+                [-1.0, 1.0, 0.0]
+            ),
+            0.75*math.pi
         )
 
 
